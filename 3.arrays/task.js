adc516b28b0719f5a7204c03d758107224a2578b
Majvis VSV <majvis.vsv@gmail.com>
2023-05-27 12:14:23 +0400

    Задание Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
-}
+  return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+};
 
 function getUsersNamesInAgeRange(users, gender) {
+
+  if ((users.length === 0) || (gender !== "женский" && gender !== "мужской" )) {
+     return 0;
+   }; 
   
-}
+  let sumAge = users.filter((person) => person.gender === gender);
+  
+  return sumAge.map((item) => item = item.age).reduce((sum, elem) => sum + elem) / sumAge.length;
+}; 
